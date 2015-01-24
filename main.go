3d468a2bd857d79a18ab33984cbd348ae498f98c
Aaron Kalin <akalin@martinisoftware.com>
2015-01-23 18:26:43 -0600

    Change to a proper HTML content type.

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func MaintenanceHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func appMaintenance(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/html; charset=UTF-8")
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
 	res.WriteHeader(http.StatusServiceUnavailable)
 	fmt.Fprintln(res, fmt.Sprintf("<html><head><title>%s</title></head><body><h1>%s</h1><p>Follow the updates at the <a href='%s'>status site</a></p></body></html>", maintenance.Message, maintenance.Message, maintenance.Href))
 }
